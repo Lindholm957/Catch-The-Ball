2d3b5d5442ec53e196c3239d322dd54ebfc43525
lindholm <yan957@gmail.com>
2019-04-13 21:21:47 +0300

    My first experience with VCS!

--- app/src/main/java/com/example/catchtheball/MainActivity.java
+++ app/src/main/java/com/example/catchtheball/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.catchtheball;
 
+import android.content.Intent;
+import android.graphics.Point;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -24,9 +28,20 @@ public class MainActivity extends AppCompatActivity {
     //Размер
     private int frameHeight;
     private int boxSize;
+    private int screenWidth;
+    private int screenHeight;
 
     //Позиция
     private int boxY;
+    private int orangeX;
+    private int orangeY;
+    private int pinkX;
+    private int pinkY;
+    private int blackX;
+    private int blackY;
+
+    //Счёт
+    private int score = 0;
 
     //Инициализируем классы
     private Handler handler = new Handler();
@@ -48,6 +63,15 @@ public class MainActivity extends AppCompatActivity {
         pink = (ImageView) findViewById(R.id.pink);
         black = (ImageView) findViewById(R.id.black);
 
+        //Получаем размер экрана
+        WindowManager wm = getWindowManager();
+        Display disp = wm.getDefaultDisplay();
+        Point size = new Point();
+        disp.getSize(size);
+
+        screenWidth = size.x;
+        screenHeight = size.y;
+
         //Переместиться за пределы экрана
         orange.setX(-80);
         orange.setY(-80);
@@ -55,12 +79,44 @@ public class MainActivity extends AppCompatActivity {
         pink.setY(-80);
         black.setX(-80);
         black.setY(-80);
+
+        scoreLabel.setText("Score: + 0");
     }
 
     public void changePos() {
 
+        hitCheck();
+        // Orange
+        orangeX -= 12;
+        if (orangeX < 0){
+            orangeX = screenWidth +20;
+            orangeY = (int) Math.floor(Math.random() * (frameHeight - orange.getHeight()));
+        }
+        orange.setX(orangeX);
+        orange.setY(orangeY);
+
+        //Black
+        blackX -= 16;
+        if (blackX < 0) {
+            blackX = screenWidth + 10;
+            blackY = (int) Math.floor(Math.random() * (frameHeight - black.getHeight()));
+        }
+
+        black.setX(blackX);
+        black.setY(blackY);
+
+        //Pink
+        pinkX -= 20;
+        if (pinkX < 0){
+            pinkX = screenWidth +5000;
+            pinkY = (int) Math.floor(Math.random() * (frameHeight - pink.getHeight()));
+        }
+
+        pink.setX(pinkX);
+        pink.setY(pinkY);
+
         //Перемещение box
-        if (action_flg ==true){
+        if (action_flg == true){
             //Touching
             boxY -= 20;
         } else  {
@@ -75,7 +131,48 @@ public class MainActivity extends AppCompatActivity {
 
         box.setY(boxY);
 
+        scoreLabel.setText("Score: "+ score);
 
+
+    }
+
+    public void hitCheck(){
+        // Если шарик в центре box, считается как попадание
+
+        //Orange
+        int orangeCenterX = orangeX + orange.getWidth()/2;
+        int orangeCenterY = orangeY + orange.getHeight()/2;
+
+        if (0 <= orangeCenterX && orangeCenterX <= boxSize &&
+                boxY <= orangeCenterY && orangeCenterY <= boxY + boxSize){
+            score += 10;
+            orangeX = -10;
+        }
+
+        //Pink
+        int pinkCenterX = pinkX + pink.getWidth()/2;
+        int pinkCenterY = pinkY + pink.getHeight()/2;
+
+        if (0 <= pinkCenterX && pinkCenterX <= boxSize &&
+                boxY <= pinkCenterY && pinkCenterY <= boxY + boxSize){
+            score += 30;
+            pinkX = -10;
+        }
+
+        //Black
+        int blackCenterX = blackX + black.getWidth()/2;
+        int blackCenterY = blackY + black.getHeight()/2;
+
+        if (0 <= blackCenterX && blackCenterX <= boxSize &&
+                boxY <= blackCenterY && blackCenterY <= boxY + boxSize){
+            timer.cancel();
+            timer = null;
+
+            //Результат!!!
+            Intent intent = new Intent(getApplicationContext(), result.class);
+            intent.putExtra("SCORE", score);
+            startActivity(intent);
+        }
     }
 
     public boolean onTouchEvent(MotionEvent me){
